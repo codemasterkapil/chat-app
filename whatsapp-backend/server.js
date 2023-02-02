470ea6793bea0e6936281253be774044c0e21518
kapil kumar sangwan <sangwankapil2020@gmail.com>
2023-02-02 13:02:22 +0530

    init

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,104 @@
+//importing
+import express from 'express';
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Pusher from 'pusher';
+import cors from "cors";
+
+// app config
+const app=express();
+
+const port=process.env.PORT || 9000;
+
+const db=mongoose.connection;
+
+db.once("open",()=>{
+    console.log('DB connected!!')
+
+    const msgCollection=db.collection("messagecontents");
+    const changeStream=msgCollection.watch();
+
+    changeStream.on("change",(change)=>{
+        console.log(change);
+
+        if(change.operationType==='insert'){
+            const messageDetails=change.fullDocument;
+            pusher.trigger("messages","inserted",{
+                name: messageDetails.name,
+                message: messageDetails.message,
+                timestamp:messageDetails.timestamp,
+                received:messageDetails.received,
+            });
+        }else{
+            console.log("error in trigerring Pusher");
+        }
+    });
+
+   
+
+
+});
+
+
+const pusher = new Pusher({
+  appId: "1544887",
+  key: "7335617ed0f2cc1af1c1",
+  secret: "86ef8c7bf18f70b7221a",
+  cluster: "ap2", 
+  useTLS: true
+});
+
+
+// middlewares
+app.use(express.json());
+
+app.use(cors());
+
+
+// Db config
+const connection_url='mongodb+srv://sangwan9:nQwaxT3ijUiFHKt@chatter.mbsdweb.mongodb.net/?retryWrites=true&w=majority'
+mongoose.set('strictQuery', false)
+mongoose.connect(connection_url, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+
+
+
+
+// ???
+
+//api routes
+app.get('/',(req,res)=>{
+    res.status(200).send('hello kapil')
+})
+
+app.get('/messages/sync',(req,res)=>{
+   
+    Messages.find((err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(data);
+        }
+    })
+});
+
+app.post('/messages/new',(req,res)=>{
+    const dbMessage=req.body;
+
+    Messages.create(dbMessage,(err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(201).send(data);
+        }
+    })
+
+})
+
+
+//listen
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
